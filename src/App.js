d2b9fe6b853ec2a07de359edff14c77ba8f7c45b
Mantvydas <mantvydas.dirgincius@icloud.comm>
2022-06-14 14:08:52 +0300

    pages folder

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import { Route } from 'react-router-dom';
 import { Switch } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
-import MedicationsPage from './components/Medications/MedicationsPage';
-import PetsPage from './components/Pets/PetsPage';
+import MedicationsPage from './components/pages/Medications/MedicationsPage';
+import PetsPage from './components/pages/Pets/PetsPage';
 
 function App() {
   return (
