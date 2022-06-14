d5d2311cb2dfc283e4e6fc5cfa1d184b9736cec4
Mantas <m.juozenas@gmail.com>
2022-06-14 15:49:41 +0300

    pet fetch

--- src/components/pages/Pets/PetsPage.jsx
+++ src/components/pages/Pets/PetsPage.jsx
@@ -1,7 +1,51 @@
+import './PetsPage.css';
+import Button from '../../UI/Button/Button.jsx';
+import { useEffect, useState } from 'react';
+import Card from '../../Card/Card';
+
 function PetsPage() {
+  const [petsData, setPetsData] = useState([]);
+
+  async function fetchPets() {
+    const resp = await fetch('petsdb.json');
+    const data = await resp.json();
+    setPetsData(data);
+  }
+
+  useEffect(() => {
+    fetchPets();
+  }, []);
+
+  function clicked(id) {
+    console.log(id);
+  }
+
   return (
-    <div>
-      <h1>PetsPage</h1>
+    <div className='container'>
+      <div className='hero'>
+        <h1>PetsPage</h1>
+        <div>
+          <Button onClick={clicked} full>
+            ADD PET
+          </Button>
+        </div>
+      </div>
+      <div className='grid'>
+        {petsData.map((obj) => (
+          <Card
+            key={obj.id}
+            title={obj.name}
+            onClick={clicked}
+            sub1={obj.dob}
+            sub2={obj.client_email}
+            id={obj.id}
+            btn={[
+              { full: true, text: 'VIEW LOG' },
+              { full: false, text: 'DELETE' },
+            ]}
+          />
+        ))}
+      </div>
     </div>
   );
 }
