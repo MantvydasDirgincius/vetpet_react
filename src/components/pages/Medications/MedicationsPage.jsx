8de22c064f67c3e9d96cd6e564dd1d302a183bc9
Mantas <m.juozenas@gmail.com>
2022-06-16 11:11:28 +0300

    search

--- src/components/pages/Medications/MedicationsPage.jsx
+++ src/components/pages/Medications/MedicationsPage.jsx
@@ -2,34 +2,66 @@ import Button from '../../UI/Button/Button.jsx';
 import { useEffect, useState } from 'react';
 import Card from '../../Card/Card';
 import { Link } from 'react-router-dom';
+import Input from '../../UI/Input/Input.jsx';
 function MedicationsPage() {
   const [medData, setMedData] = useState([]);
+  const [visibleMeds, setVisibleMeds] = useState([]);
+  const [inputValue, setInputValue] = useState('');
 
   async function getMed() {
     const resp = await fetch('https://glittery-dull-snickerdoodle.glitch.me/v1/meds/');
     const data = await resp.json();
-
     setMedData(data);
+    setVisibleMeds(data);
   }
   useEffect(() => {
     getMed();
   }, []);
+  // mObj.name.toLowerCase().includes(inputValue.toLowerCase())
+  function handleInput() {
+    const filtered = medData.filter((mObj) => {
+      if (!mObj.name) {
+        return;
+      }
+      return mObj.name.toLowerCase().includes(inputValue.toLowerCase());
+    });
+    setVisibleMeds(filtered);
+  }
+
+  useEffect(() => {
+    handleInput();
+  }, [inputValue]);
 
   return (
     <div className='container'>
       <div className='hero'>
         <h1>Medications</h1>
+        <Input
+          type='text'
+          placeholder='Search'
+          onChange={(e) => setInputValue(e.target.value)}
+          value={inputValue}
+          name='search'
+        />
         <div>
           <Link to='/addMeds'>
             <Button full>ADD MEDICATIONS</Button>
           </Link>
         </div>
       </div>
-      <div className='grid'>
-        {medData.map((obj) => (
-          <Card key={obj.id} title={obj.name} sub1={obj.description} />
-        ))}
-      </div>
+      {inputValue.length === 0 ? (
+        <div className='grid'>
+          {medData.map((obj) => (
+            <Card key={obj.id} title={obj.name} sub1={obj.description} />
+          ))}
+        </div>
+      ) : (
+        <div className='grid'>
+          {visibleMeds.map((obj) => (
+            <Card key={obj.id} title={obj.name} sub1={obj.description} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
--- src/components/pages/Pets/PetsPage.jsx
+++ src/components/pages/Pets/PetsPage.jsx
@@ -2,14 +2,27 @@ import Button from '../../UI/Button/Button.jsx';
 import { useEffect, useState } from 'react';
 import Card from '../../Card/Card';
 import { Link } from 'react-router-dom';
+import Input from '../../UI/Input/Input.jsx';
 
 function PetsPage() {
   const [petsData, setPetsData] = useState([]);
+  const [visiblePets, setVisiblePets] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  function handleInput() {
+    const filtered = petsData.filter((pObj) => pObj.name.toLowerCase().includes(inputValue.toLowerCase()));
+    setVisiblePets(filtered);
+  }
+
+  useEffect(() => {
+    handleInput();
+  }, [inputValue]);
 
   async function fetchPets() {
     const resp = await fetch('https://glittery-dull-snickerdoodle.glitch.me/v1/pets');
     const data = await resp.json();
     setPetsData(data);
+    setVisiblePets(data);
   }
   useEffect(() => {
     fetchPets();
@@ -29,27 +42,53 @@ function PetsPage() {
     <div className='container'>
       <div className='hero'>
         <h1>PetsPage</h1>
+        <Input
+          type='text'
+          placeholder='Search'
+          onChange={(e) => setInputValue(e.target.value)}
+          value={inputValue}
+          name='search'
+        />
         <div>
           <Link to='/addPets'>
             <Button full>ADD PET</Button>
           </Link>
         </div>
       </div>
-      <div className='grid'>
-        {petsData.map((obj, i) => (
-          <Card
-            key={i}
-            title={obj.name}
-            sub1={new Date(obj.dob).toDateString()}
-            sub2={obj.client_email}
-            id={obj.id}
-            btn={[
-              { full: true, text: 'VIEW LOG', link: true, name: obj.name },
-              { full: false, text: 'DELETE', onDelete: deleteById },
-            ]}
-          />
-        ))}
-      </div>
+
+      {inputValue.length === 0 ? (
+        <div className='grid'>
+          {petsData.map((obj, i) => (
+            <Card
+              key={i}
+              title={obj.name}
+              sub1={new Date(obj.dob).toDateString()}
+              sub2={obj.client_email}
+              id={obj.id}
+              btn={[
+                { full: true, text: 'VIEW LOG', link: true, name: obj.name },
+                { full: false, text: 'DELETE', onDelete: deleteById },
+              ]}
+            />
+          ))}
+        </div>
+      ) : (
+        <div className='grid'>
+          {visiblePets.map((obj, i) => (
+            <Card
+              key={i}
+              title={obj.name}
+              sub1={new Date(obj.dob).toDateString()}
+              sub2={obj.client_email}
+              id={obj.id}
+              btn={[
+                { full: true, text: 'VIEW LOG', link: true, name: obj.name },
+                { full: false, text: 'DELETE', onDelete: deleteById },
+              ]}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
